33741e2a2cb820cbe5df910527ffa17cb7c8da61
kevin kaneza <65622766+kevindermot99@users.noreply.github.com>
2024-02-18 07:48:24 +0200

    navbar UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,34 @@ import './App.css'
 import Ui from './pages/ui'
 import memoji from '../public/memoji.jpeg'
 
+// icons
+import { FaGithub } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { IoLogoWhatsapp } from "react-icons/io";
+
 function App() {
 
   return (
     <>
+    {/* navbar */}
     <div className="navbar">
       <div className="n-memoji">
         <img src={memoji} alt="" />
       </div>
       <div className="n-links">
-      <span>UI\UX</span>
-        <span>Fullstack</span>
+        <span>UI\UX</span>
+        <span>Full Stack</span>
+        <span>skills</span>
+        <span>About me</span>
       </div>
-      <div className="n-menu"></div>
+      <div className="n-socials">
+        <a href="#"><FaGithub /></a>
+        <a href="#"><SiGmail /></a>
+        <a href="#"><IoLogoWhatsapp />  </a>
+      </div>
     </div>
-    <Ui />
+    
+
     </>
   )
 }
