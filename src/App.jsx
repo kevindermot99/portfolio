06c7b4e47ee9f147c20c2507720d3b4dac7718fa
kevin kaneza <65622766+kevindermot99@users.noreply.github.com>
2024-02-19 19:43:18 +0200

    memoji card UI

--- src/App.jsx
+++ src/App.jsx
@@ -13,36 +13,45 @@ function App() {
 
   return (
     <div className='wrapper'>
-    <div className="bubble1"></div>
-    <div className="bubble2"></div>
-    {/* navbar */}
-    <div className="navbar">
-      <div className="n-memoji">
-        <img src={memoji} alt="" />
-      </div>
-      <div className="n-links">
-        <span>UI\UX</span>
-        <span>Full Stack</span>
-        <span>skills</span>
-        <span>About me</span>
-      </div>
-      <div className="n-socials">
-        <a href="#"><FaGithub /></a>
-        <a href="#"><SiGmail /></a>
-        <a href="#"><IoLogoWhatsapp />  </a>
-      </div>
-    </div>
+      <div className="bubble1"></div>
+      <div className="bubble2"></div>
 
 
-    <div className="hero">
-      <h1>Hi, I'm Kevin.</h1>
-      <p><SlLocationPin /> Kigali, Rwanda </p>
-      <p>I am currently pursuing a Bachelor's degree in Software Engineering at <a href="https://www.alueducation.com" target='_blank'>ALU</a>. For further information about my academic background, check this page <a href="#"> about me</a></p>
-      {/* <p>I'm curently at <a href="#">ALU</a> taking Bachelors of Software Engineering, for more info <a href="#">About Me.</a></p> */}
-    </div>
+      {/* navbar */}
+      <div className="navbar">
 
-    
-    <div className="home"></div>
+        <div className="n-memoji">
+          <img src={memoji} alt="" />
+          <div className="n-memoji-box">
+            
+          </div>
+        </div>
+
+        <div className="n-links">
+          <span>UI\UX</span>
+          <span>Full Stack</span>
+          <span>skills</span>
+          <span>About me</span>
+        </div>
+
+        <div className="n-socials">
+          <a href="#"><FaGithub /></a>
+          <a href="#"><SiGmail /></a>
+          <a href="#"><IoLogoWhatsapp />  </a>
+        </div>
+
+      </div>
+
+
+      <div className="hero">
+        <h1>Hi, I'm Kevin.</h1>
+        <p><SlLocationPin /> Kigali, Rwanda </p>
+        <p>I am currently pursuing a Bachelor's degree in Software Engineering at <a href="https://www.alueducation.com" target='_blank'>ALU</a>. For further information about my academic background, check this page <a href="#"> about me</a></p>
+        {/* <p>I'm curently at <a href="#">ALU</a> taking Bachelors of Software Engineering, for more info <a href="#">About Me.</a></p> */}
+      </div>
+
+
+      <div className="home"></div>
     </div>
   )
 }
