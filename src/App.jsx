aa5024fc183f0936729ec5e2184f630b0a05afd8
kevindermot99 <mbonimpayekevin@gmail.com>
2024-03-13 11:12:00 +0200

    projects grid complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './App.css'
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Projects from './pages/Projects'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div>
+      {/* <Navbar /> */}
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Projects />}></Route>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,14 +5,37 @@ import Navbar from '../components/Navbar'
 function Projects() {
 
     return (
-        <section>
+        <div>
             <Navbar />
-            <div className='flex items-center justify-center flex-col my-5 '>
-                <h1 className='text-5xl text-black font-bold mb-2'>{Greetings}!</h1>
-                <p className='text-sm text-black'>{NameAndLocation}</p>
-            </div>
 
-        </section>
+            <section className='flex items-center justify-center flex-col my-5 '>
+                <h1 className='text-5xl text-black font-bold mb-2'>{Greetings}!</h1>
+                <p className='text-sm text-black '>{NameAndLocation}</p>
+            </section>
+
+            <section className='w-full max-w-[1100px] grid grid-cols-2 gap-2 m-auto pt-10'>
+                <div className='w-[100%] h-[300px] bg-orange-500'>
+
+                </div>
+                <div className='w-[100%] h-[300px] bg-purple-500'>
+
+                </div>
+
+                <div className='w-[100%] h-[300px] bg-red-500'>
+
+                </div>
+                <div className='w-[100%] h-[300px] bg-lime-500'>
+
+                </div>
+
+                <div className='w-[100%] h-[300px] bg-amber-500'>
+
+                </div>
+                <div className='w-[100%] h-[300px] bg-violet-500'>
+
+                </div>
+            </section>
+        </div>
     )
 }
 
