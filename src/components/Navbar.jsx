b4723a985c6f3d16a8e1a85f4e1406f460a1001d
kevindermot99 <mbonimpayekevin@gmail.com>
2024-03-13 11:23:23 +0200

    name in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,8 @@ function Navbar() {
   return (
     <div className='bg-white bg-opacity-70 backdrop-blur-xl px-7 py-2 h-fit w-full flex items-center justify-between'>
         <img src={memoji} className="h-10 rounded-full" />
+
+        <h1 className='text-xl font-bold'>mboni Kevin</h1>
         
         <div className='flex gap-3 items-center justify-center'>
             <FaGithub className='text-3xl text-black cursor-pointer' />
