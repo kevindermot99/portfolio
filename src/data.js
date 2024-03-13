7cbf6912c7140b11bfa82b3ef2b259ae458f5da1
kevindermot99 <mbonimpayekevin@gmail.com>
2024-03-13 11:22:23 +0200

    dock init

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
 export const Greetings = "Hello"
-export const NameAndLocation = "I'm Kevin, a Rwandan-based Web application Developer"
+export const NameAndLocation = "I'm Kevin, a Rwandan-based Web application Developer currently at African Leadership University in BSC. (HONS) SOFTWARE ENGINEERING"
 
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { Greetings, NameAndLocation } from '../data'
 import Navbar from '../components/Navbar'
+import Dock from '../components/Dock'
 
 function Projects() {
 
     return (
         <div>
             <Navbar />
+            <Dock />
 
             <section className='flex items-center justify-center flex-col my-5 '>
                 <h1 className='text-5xl text-black font-bold mb-2'>{Greetings}!</h1>
-                <p className='text-sm text-black '>{NameAndLocation}</p>
+                <p className='text-sm text-black max-w-[600px] text-center '>{NameAndLocation}</p>
             </section>
 
-            <section className='w-full max-w-[1100px] grid grid-cols-2 gap-2 m-auto pt-10'>
+            <section className='w-full max-w-[1150px] grid grid-cols-2 gap-1 m-auto p-10'>
                 <div className='w-[100%] h-[300px] bg-orange-500'>
 
                 </div>
