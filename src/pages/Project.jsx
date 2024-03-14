6f87c4de55c569c21cbb039e343dc2c7de863a1d
kevindermot99 <mbonimpayekevin@gmail.com>
2024-03-14 11:35:21 +0200

    project banner complete

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -5,6 +5,7 @@ import { IoChevronBack } from "react-icons/io5";
 import { IoShareSocialOutline } from "react-icons/io5";
 import { IoCheckmarkOutline } from "react-icons/io5";
 import { useState } from 'react'
+import Navbar from '../components/Navbar';
 
 function Project() {
     const { id } = useParams();
@@ -28,16 +29,24 @@ function Project() {
         return <div>Item not found!</div>
     }
     return (
-        <div className='max-w-[1200px] m-auto'>
-            <div className=' relative flex justify-between items-center py-3'>
-                <Link to="/" className='text-xl p-2 bg-gray-100 hover:bg-gray-200 rounded-xl'><IoChevronBack /></Link>
-                <button className='text-xl p-2 bg-gray-100 hover:bg-gray-200 outline-none rounded-xl' onClick={copylink}><IoShareSocialOutline /></button>
-                <div className={`absolute right-0 -bottom-[40px] bg-gray-100 rounded-xl p-3 flex items-center justify-start transition-all   ${copied ? ' opacity-100 scale-100' : ' opacity-0 scale-0'}`} id='copied'>
-                    <IoCheckmarkOutline className='text-xl text-green-600' />
-                    <p className='text-sm px-1 font-medium text-gray-500'>
-                        Link Copied
-                    </p>
+        <div>
+            <Navbar />
+
+            <div className='w-full relative px-7 max-w-[1200px] m-auto'>
+
+                <div className='w-full h-[300px] rounded-xl bg-gray-200 relative'>
+                    <Link to="/" className='text-xl p-2 bg-gray-100 hover:scale-105 bg-opacity-70 backdrop-blur-md shadow-sm rounded-xl absolute top-3 left-3'><IoChevronBack /></Link>
+                    <button className='text-xl p-2 bg-gray-100 hover:scale-105 bg-opacity-70 backdrop-blur-md shadow-sm outline-none rounded-xl absolute top-3 right-3' onClick={copylink}><IoShareSocialOutline /></button>
                 </div>
+
+            </div>
+
+
+            <div className={`fixed left-0 right-0 bottom-4 m-auto w-fit bg-gray-200 rounded-xl p-3 flex items-center justify-start transition-all   ${copied ? ' opacity-100 scale-100' : ' opacity-0 scale-0'}`} id='copied'>
+                <IoCheckmarkOutline className='text-xl text-green-600' />
+                <p className='text-sm px-1 font-medium text-gray-500'>
+                    Link Copied
+                </p>
             </div>
         </div>
     )
