f628d83a3731353eb37e862339920f9b1fd5bf50
kevindermot99 <mbonimpayekevin@gmail.com>
2024-03-14 08:59:08 +0200

    sample data object1 complete

--- src/data.js
+++ src/data.js
@@ -1,3 +1,11 @@
+// import images
+import example1 from "./assets/thumbnails/example1.jpg"
+import example2 from "./assets/thumbnails/example2.png"
+import example3 from "./assets/thumbnails/example3.jpg"
+import example4 from "./assets/thumbnails/example4.jpg"
+import example5 from "./assets/thumbnails/example5.png"
+import example6 from "./assets/thumbnails/example6.jpg"
+
 export const Greetings = "Hello"
 export const Situation = "I am Kevin, a Web Application Developer based in Rwanda, currently pursuing a BSc (Hons) in Software Engineering at African Leadership University."
 // export const 
@@ -15,6 +23,9 @@ export const NavLinks = [
 const projectModel = [
     {
         id: "8p3hwvf64q1nix1q2fzd",
-        imageURL: ""
+        thumbnail: {example1},
+        title: "THE App Clone hehheehhhhehehe",
+        description: "suka buryaat",
+        liveDemoLink: "www.google.com",
     }
 ]
