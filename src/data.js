45d530875fdc9d291af240c748de294bc40fb078
kevindermot99 <mbonimpayekevin@gmail.com>
2024-06-01 21:09:37 +0200

    projectModel new Skeleton

--- src/data.js
+++ src/data.js
@@ -1,252 +1,118 @@
-
-// import technologies
-import Html from "./assets/technologies/icons8-html-240.png"
-import Css from "./assets/technologies/icons8-css-240.png"
-import Express from "./assets/technologies/icons8-express-js-250.png"
-import Js from "./assets/technologies/icons8-javascript-240.png"
-import Mongo from "./assets/technologies/icons8-mongodb-240.png"
-import Next from "./assets/technologies/icons8-next.js-240.png"
-import NodeJS from "./assets/technologies/icons8-nodejs-240.png"
-import React from "./assets/technologies/icons8-react-native-68.png"
-import Tailwind from "./assets/technologies/icons8-tailwindcss-240.png"
-
-
-
 export const Greetings = "Hello, I'm Kevin."
 export const AboutMe = "a Web Developer based in Rwanda. Currently pursuing a BSc (Hons) in Software Engineering at African Leadership University."
 
-export const NavNames = 'mboni kevin.'
+export const NavNames = 'Mboni Kevin.'
 
 
 
 // projects
 export const ProjectModel = [
     {
-        id: "",
-        thumbnail: '',
-        wallBg: "",
-        title: "",
-        slogan: "",
-        description: "",
-        liveDemoLink: "",
-        technologies: [
-            {  },
-            {  },
-            {  },
-            {  },
-            { }
-        ],
-        duration: '',
-        linkSourceCode: '',
-        role: ''
+        id: "1",
+        thumbnail: "",
+        title: "Gamezilla",
+        liveDemoLink: "/",
+        type: "Full-Stack",
+        technologies: ["React", "Node JS", "Tailwind CSS", "MongoDB", "Express JS"],
+        due: "",
     },
     {
         id: "",
-        thumbnail: '',
-        wallBg: "",
+        thumbnail: "",
         title: "",
-        slogan: "",
-        description: "",
-        liveDemoLink: "",
-        technologies: [
-            {  },
-            {  },
-            {  },
-            {  },
-            { }
-        ],
-        duration: '',
-        linkSourceCode: '',
-        role: ''
-    },
+        liveDemoLink: "/",
+        type: "",
+        technologies: [""],
+        due: "",
+    },,
     {
         id: "",
-        thumbnail: '',
-        wallBg: "",
+        thumbnail: "",
         title: "",
-        slogan: "",
-        description: "",
-        liveDemoLink: "",
-        technologies: [
-            {  },
-            {  },
-            {  },
-            {  },
-            { }
-        ],
-        duration: '',
-        linkSourceCode: '',
-        role: ''
-    },
+        liveDemoLink: "/",
+        type: "",
+        technologies: [""],
+        due: "",
+    },,
     {
         id: "",
-        thumbnail: '',
-        wallBg: "",
+        thumbnail: "",
         title: "",
-        slogan: "",
-        description: "",
-        liveDemoLink: "",
-        technologies: [
-            {  },
-            {  },
-            {  },
-            {  },
-            { }
-        ],
-        duration: '',
-        linkSourceCode: '',
-        role: ''
-    },
+        liveDemoLink: "/",
+        type: "",
+        technologies: [""],
+        due: "",
+    },,
     {
         id: "",
-        thumbnail: '',
-        wallBg: "",
+        thumbnail: "",
         title: "",
-        slogan: "",
-        description: "",
-        liveDemoLink: "",
-        technologies: [
-            {  },
-            {  },
-            {  },
-            {  },
-            { }
-        ],
-        duration: '',
-        linkSourceCode: '',
-        role: ''
-    },
+        liveDemoLink: "/",
+        type: "",
+        technologies: [""],
+        due: "",
+    },,
     {
         id: "",
-        thumbnail: '',
-        wallBg: "",
+        thumbnail: "",
         title: "",
-        slogan: "",
-        description: "",
-        liveDemoLink: "",
-        technologies: [
-            {  },
-            {  },
-            {  },
-            {  },
-            { }
-        ],
-        duration: '',
-        linkSourceCode: '',
-        role: ''
-    },
+        liveDemoLink: "/",
+        type: "",
+        technologies: [""],
+        due: "",
+    },,
     {
         id: "",
-        thumbnail: '',
-        wallBg: "",
+        thumbnail: "",
         title: "",
-        slogan: "",
-        description: "",
-        liveDemoLink: "",
-        technologies: [
-            {  },
-            {  },
-            {  },
-            {  },
-            { }
-        ],
-        duration: '',
-        linkSourceCode: '',
-        role: ''
-    },
+        liveDemoLink: "/",
+        type: "",
+        technologies: [""],
+        due: "",
+    },,
     {
         id: "",
-        thumbnail: '',
-        wallBg: "",
+        thumbnail: "",
         title: "",
-        slogan: "",
-        description: "",
-        liveDemoLink: "",
-        technologies: [
-            {  },
-            {  },
-            {  },
-            {  },
-            { }
-        ],
-        duration: '',
-        linkSourceCode: '',
-        role: ''
-    },
+        liveDemoLink: "/",
+        type: "",
+        technologies: [""],
+        due: "",
+    },,
     {
         id: "",
-        thumbnail: '',
-        wallBg: "",
+        thumbnail: "",
         title: "",
-        slogan: "",
-        description: "",
-        liveDemoLink: "",
-        technologies: [
-            {  },
-            {  },
-            {  },
-            {  },
-            { }
-        ],
-        duration: '',
-        linkSourceCode: '',
-        role: ''
-    },
+        liveDemoLink: "/",
+        type: "",
+        technologies: [""],
+        due: "",
+    },,
     {
         id: "",
-        thumbnail: '',
-        wallBg: "",
+        thumbnail: "",
         title: "",
-        slogan: "",
-        description: "",
-        liveDemoLink: "",
-        technologies: [
-            {  },
-            {  },
-            {  },
-            {  },
-            { }
-        ],
-        duration: '',
-        linkSourceCode: '',
-        role: ''
-    },
+        liveDemoLink: "/",
+        type: "",
+        technologies: [""],
+        due: "",
+    },,
     {
         id: "",
-        thumbnail: '',
-        wallBg: "",
+        thumbnail: "",
         title: "",
-        slogan: "",
-        description: "",
-        liveDemoLink: "",
-        technologies: [
-            {  },
-            {  },
-            {  },
-            {  },
-            { }
-        ],
-        duration: '',
-        linkSourceCode: '',
-        role: ''
-    },
+        liveDemoLink: "/",
+        type: "",
+        technologies: [""],
+        due: "",
+    },,
     {
         id: "",
-        thumbnail: '',
-        wallBg: "",
+        thumbnail: "",
         title: "",
-        slogan: "",
-        description: "",
-        liveDemoLink: "",
-        technologies: [
-            {  },
-            {  },
-            {  },
-            {  },
-            { }
-        ],
-        duration: '',
-        linkSourceCode: '',
-        role: ''
+        liveDemoLink: "/",
+        type: "",
+        technologies: [""],
+        due: "",
     },
 ]
