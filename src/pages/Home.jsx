36f81bc25d46bab05e3aaf075fb4dd758071bec0
kevindermot99 <mbonimpayekevin@gmail.com>
2024-07-04 14:50:41 +0200

    services 2 cards complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,11 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { CiLocationOn } from "react-icons/ci";
 import { TbArrowCurveLeft } from "react-icons/tb";
-import { MdArrowRightAlt } from "react-icons/md";
+import { MdArrowRightAlt, MdOutlineErrorOutline } from "react-icons/md";
+import { RxSpaceBetweenVertically } from "react-icons/rx";
+import { PiSelectionPlusDuotone } from "react-icons/pi";
+import { GoShieldCheck } from "react-icons/go";
+import { RiSignalWifiErrorLine } from "react-icons/ri";
 
 function Home() {
   const today = new Date();
@@ -88,11 +92,28 @@ function Home() {
         </div>
 
         <div className="w-full h-fit flex gap-6 mt-6">
-          <div className="group w-1/2 h-[210px] ring-1 ring-stone-300/70 dark:ring-stone-700/30 transition hover:bg-stone-300/30 dark:hover:bg-stone-700/20 rounded-2xl p-8 flex flex-col items-start justify-between">
-            <div className="font-bold text-[40px] text-black/20 dark:text-white/30 group-hover:text-orange-500 transition">
+          <div className="group w-1/2 h-[210px] ring-1 ring-stone-300/70 dark:ring-stone-700/30 transition hover:bg-stone-300/30 dark:hover:bg-stone-700/20 rounded-2xl p-8 flex flex-col items-start justify-between relative overflow-clip">
+            {/* element */}
+            <div className="text-stone-400 dark:text-stone-700 absolute right-0 top-0 h-full min-w-[100px] translate-x-5 group-hover:translate-x-0 -rotate-[7deg] group-hover:-rotate-[10deg] bg-stone-100/0 p-5 flex items-center justify-center flex-col gap-3 -z-0 opacity-40 transition duration-300 group-hover:opacity-100 ">
+              <div className="h-[60px] w-[200px] rounded-xl ring-1 ring-stone-400 dark:ring-stone-700 relative p-5 flex items-center justify-start">
+                <span className="absolute bottom-[-2px] left-[-2px] h-[20px] w-[20px] border-b-[3px] border-l-[3px] border-orange-500 rounded-bl-2xl "></span>
+                <span className="absolute top-[-2px] right-[-2px] h-[20px] w-[20px] border-t-[3px] border-r-[3px] border-orange-500 rounded-tr-2xl "></span>
+                <PiSelectionPlusDuotone className="text-3xl cursor-pointer transition group-hover:text-orange-500" />
+              </div>
+              <div className="w-full flex items-center justify-center">
+                <RxSpaceBetweenVertically className=" text-xl" />
+              </div>
+              <div className="h-[60px] w-[230px] rounded-xl ring-1 ring-stone-400 dark:ring-stone-700 relative p-5 flex items-center justify-end">
+                <span className="absolute bottom-[-2px] left-[-2px] h-[20px] w-[20px] border-b-[3px] border-l-[3px] border-orange-500 rounded-bl-2xl "></span>
+                <span className="absolute top-[-2px] right-[-2px] h-[20px] w-[20px] border-t-[3px] border-r-[3px] border-orange-500 rounded-tr-2xl "></span>
+                <PiSelectionPlusDuotone className="text-4xl cursor-pointer transition group-hover:text-orange-500" />
+              </div>
+            </div>
+            {/* content */}
+            <div className="font-bold text-[40px] text-black/20 dark:text-white/30 group-hover:text-orange-500 transition z-10">
               01
             </div>
-            <div className="flex flex-col items-start justify-start">
+            <div className="flex flex-col items-start justify-start z-10">
               <h1 className="text-2xl font-medium tracking-tight capitalize ">
                 Front-end Development
               </h1>
@@ -101,7 +122,55 @@ function Home() {
               </p>
             </div>
           </div>
-          <div className="group w-1/2 h-[210px] ring-1 ring-stone-300/70 dark:ring-stone-700/30 transition hover:bg-stone-300/30 dark:hover:bg-stone-700/20 rounded-2xl p-8 flex flex-col items-start justify-between">
+          <div className="group w-1/2 h-[210px] ring-1 ring-stone-300/70 dark:ring-stone-700/30 transition hover:bg-stone-300/30 dark:hover:bg-stone-700/20 rounded-2xl p-8 flex flex-col items-start justify-between relative overflow-clip">
+            {/* element */}
+            <div className="text-stone-400 dark:text-stone-700 absolute right-0 top-0 h-full min-w-[100px] translate-x-5 group-hover:translate-x-0 -rotate-[7deg] group-hover:-rotate-[10deg] bg-stone-100/0 p-5 flex items-center justify-center flex-col gap-3 -z-0 opacity-40 transition duration-300 group-hover:opacity-100 ">
+              <div className="h-[60px] w-[210px] rounded-xl ring-1 group-hover:text-black/90 dark:group-hover:text-white/60 ring-stone-400 dark:ring-stone-700 hover:bg-stone-300/40 transition dark:hover:bg-stone-800 cursor-pointer relative py-2 px-4 flex items-center justify-start gap-2 overflow-clip">
+                <GoShieldCheck className="text-orange-500 text-2xl min-h-fit " />
+                <div className="flex flex-col items-start justify-start max-w-[80%]">
+                  <h1 className="text-sm font-medium line-clamp-1">
+                    Request Completed
+                  </h1>
+                  <p className="line-clamp-1 text-xs ">
+                    Your request has been successfully processed
+                  </p>
+                </div>
+              </div>
+              <div className="h-[60px] w-[210px] -translate-x-11 rounded-xl ring-1 group-hover:text-black/90 dark:group-hover:text-white/60 ring-stone-400 dark:ring-stone-700 hover:bg-stone-300/40 transition dark:hover:bg-stone-800 cursor-pointer relative py-2 px-4 flex items-center justify-start gap-2 overflow-clip">
+                <MdOutlineErrorOutline className="text-orange-500 text-2xl min-h-fit " />
+                <div className="flex flex-col items-start justify-start max-w-[80%]">
+                  <h1 className="text-sm font-medium line-clamp-1">
+                    Oops! Bad Gateway
+                  </h1>
+                  <p className="line-clamp-1 text-xs ">
+                    Something went wrong on the server side
+                  </p>
+                </div>
+              </div>
+              <div className="h-[60px] w-[210px] rounded-xl ring-1 group-hover:text-black/90 dark:group-hover:text-white/60 ring-stone-400 dark:ring-stone-700 hover:bg-stone-300/40 transition dark:hover:bg-stone-800 cursor-pointer relative py-2 px-4 flex items-center justify-start gap-2 overflow-clip">
+                <RiSignalWifiErrorLine className="text-orange-500 text-2xl min-h-fit " />
+                <div className="flex flex-col items-start justify-start max-w-[80%]">
+                  <h1 className="text-sm font-medium line-clamp-1">
+                    Connection Issue
+                  </h1>
+                  <p className="line-clamp-1 text-xs ">
+                    We’re having trouble reaching the database
+                  </p>
+                </div>
+              </div>
+              <div className="h-[60px] w-[210px] -translate-x-11 rounded-xl ring-1 group-hover:text-black/90 dark:group-hover:text-white/60 ring-stone-400 dark:ring-stone-700 hover:bg-stone-300/40 transition dark:hover:bg-stone-800 cursor-pointer relative py-2 px-4 flex items-center justify-start gap-2 overflow-clip">
+                <GoShieldCheck className="text-orange-500 text-2xl min-h-fit " />
+                <div className="flex flex-col items-start justify-start max-w-[80%]">
+                  <h1 className="text-sm font-medium line-clamp-1">
+                    Request Completed
+                  </h1>
+                  <p className="line-clamp-1 text-xs ">
+                    Your request has been successfully processed
+                  </p>
+                </div>
+              </div>
+            </div>
+            {/* content */}
             <div className="font-bold text-[40px] text-black/20 dark:text-white/30 group-hover:text-orange-500 transition">
               02
             </div>
@@ -116,7 +185,24 @@ function Home() {
           </div>
         </div>
         <div className="w-full h-fit flex gap-6">
-          <div className="group w-full h-[210px] ring-1 ring-stone-300/70 dark:ring-stone-700/30 transition hover:bg-stone-300/30 dark:hover:bg-stone-700/20 rounded-2xl p-8 flex flex-col items-start justify-between">
+          <div className="group w-full h-[210px] ring-1 ring-stone-300/70 dark:ring-stone-700/30 transition hover:bg-stone-300/30 dark:hover:bg-stone-700/20 rounded-2xl p-8 flex flex-col items-start justify-between relative overflow-clip">
+            {/* element */}
+            <div className="text-stone-400 dark:text-stone-700 absolute right-0 top-0 h-full min-w-[100px] translate-x-5 group-hover:translate-x-0 -rotate-[7deg] group-hover:-rotate-[10deg] bg-stone-100/0 p-5 flex items-center justify-center flex-col gap-3 -z-0 opacity-40 transition duration-300 group-hover:opacity-100 ">
+              <div className="h-[60px] w-[200px] rounded-xl ring-1 ring-stone-400 dark:ring-stone-700 relative p-5 flex items-center justify-start">
+                <span className="absolute bottom-[-2px] left-[-2px] h-[20px] w-[20px] border-b-[3px] border-l-[3px] border-orange-500 rounded-bl-2xl "></span>
+                <span className="absolute top-[-2px] right-[-2px] h-[20px] w-[20px] border-t-[3px] border-r-[3px] border-orange-500 rounded-tr-2xl "></span>
+                <PiSelectionPlusDuotone className="text-3xl cursor-pointer hover:text-orange-500" />
+              </div>
+              <div className="w-full flex items-center justify-center">
+                <RxSpaceBetweenVertically className=" text-xl" />
+              </div>
+              <div className="h-[60px] w-[230px] rounded-xl ring-1 ring-stone-400 dark:ring-stone-700 relative p-5 flex items-center justify-end">
+                <span className="absolute bottom-[-2px] left-[-2px] h-[20px] w-[20px] border-b-[3px] border-l-[3px] border-orange-500 rounded-bl-2xl "></span>
+                <span className="absolute top-[-2px] right-[-2px] h-[20px] w-[20px] border-t-[3px] border-r-[3px] border-orange-500 rounded-tr-2xl "></span>
+                <PiSelectionPlusDuotone className="text-4xl cursor-pointer hover:text-orange-500" />
+              </div>
+            </div>
+            {/* content */}
             <div className="font-bold text-[40px] text-black/20 dark:text-white/30 group-hover:text-orange-500 transition">
               03
             </div>
