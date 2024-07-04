7dc1656559cb3660bb58fc2bb6f44fca5d611c8c
kevindermot99 <mbonimpayekevin@gmail.com>
2024-07-04 11:20:28 +0200

    data cleaned

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { HeroText, AboutMe, ProjectModel } from "../data";
+import { HeroText, MyLocation, Promising } from "../data";
 import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
@@ -15,13 +15,13 @@ function Home() {
       {/* light */}
       <div className="w-[700px] aspect-square rounded-full bg-gradient-to-br from-stone-800/20 to-transparent dark:from-stone-200/10 blur-[90px] absolute left-[-240px] top-[-250px] z-0"></div>
 
-      <section className=" flex items-start justify-end flex-col gap-4 py-20 px-20 max-md:px-10 h-svh select-none">
-        <p className="text-base opacity-50 z-10">kigali, Rwanda</p>
-        <h1 className="text-[90px] leading-[95px] text-transparent bg-clip-text bg-gradient-to-br max-w-[800px] from-black dark:from-white to-stone-700/50 dark:to-black/80 font-extrabold tracking-tight z-10 ">
+      <section className=" flex items-start justify-end flex-col gap-2 py-10 px-20 max-md:px-10 h-svh max-h-[800px]  select-none">
+        <p className="text-base opacity-50 z-10">{MyLocation}</p>
+        <h1 className="text-[70px] leading-[75px] text-transparent bg-clip-text bg-gradient-to-br max-w-[600px] from-black dark:from-white to-stone-700/70 dark:to-black/80 font-semibold tracking-tight z-10 ">
           {HeroText}
         </h1>
         <p className="self-end text-base font-medium  select-none capitalize">
-          <s className="opacity-50 ">complexity</s>&nbsp; simplicity
+          <s className="opacity-50 ">{Promising.no}</s>&nbsp; {Promising.yes}.
         </p>
       </section>
     </div>
