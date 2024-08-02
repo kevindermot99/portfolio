795b718c3b49185c6e18cb76f3e93608138ccf3c
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-02 15:49:13 +0200

    routing resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
-import { HashRouter, Routes, Route, useFetcher } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ScrollToTop from "./ScrollToTop";
 import Work from "./pages/Work";
 import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div>
-      <HashRouter>
+      <Router>
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/work" element={<Work />}></Route>
           <Route path="/about" element={<About />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Routes>
-      </HashRouter>
+      </Router>
     </div>
   );
 }
