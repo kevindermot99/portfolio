ad0905a393b014a4bb34e766825ce654ca6adcaa
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-02 16:53:13 +0200

    new section of projects

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -26,19 +26,114 @@ function Work() {
     <div className="wrapper overflow-clip bg-light-body dark:bg-dark-body text-black/90 dark:text-white/80  ">
       <Navbar />
 
-      {/* Projects */}
+      {/* Functional Projects */}
+      <section className="w-full h-fit flex items-start justify-strat flex-col gap-2 px-20 max-md:px-5 pt-32 ">
+        <div className="grid grid-cols-2 max-md:grid-cols-1 gap-6 mb-20 max-md:mb-10 max-md:gap-5">
+          <Fade triggerOnce>
+            <h1 className="text-[37px] leading-[41px] max-md:text-[27px] max-md:leading-7  text-transparent font-semibold tracking-tight z-10 max-w-[400px] ">
+              <span className="font-medium text-base text-main-color">
+              Completed &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp;{" "}
+              </span>
+              <span className="bg-clip-text bg-gradient-to-br from-black dark:from-white to-stone-800/70 dark:to-stone-400/80">
+                Complete and Functional Projects
+              </span>
+            </h1>
+            <p className="text-base leading-6 opacity-70">
+              Here, you’ll find projects that not only look great but also works. This includes everything from full stack web apps to functional React-based apps that perform as intended.
+            </p>
+          </Fade>
+        </div>
+
+        <div className="gridRespo">
+          <Reveal
+            keyframes={customAnimation}
+            triggerOnce
+            duration={800}
+            cascade
+            damping={0.1}
+            className="flex items-start justify-end flex-col gap-2 w-full"
+          >
+            {Projects.map((project, index) => (
+              <ProjectContainer
+                name={project.name}
+                duration={project.duration}
+                img={project.cover}
+                logo={project.logo}
+                tech={project.tech}
+                url={project.url}
+              />
+            ))}
+
+            <ProjectContainer
+              name={"InvestorGraph"}
+              duration={"15 days"}
+              img={
+                "https://cdn.dribbble.com/userupload/10876856/file/original-9daa79240100c14c9a7193efa7fa746d.png?resize=320x240&vertical=center"
+              }
+            />
+            <ProjectContainer
+              name={"NFTX"}
+              duration={"2 months"}
+              img={
+                "https://cdn.dribbble.com/userupload/9645187/file/original-ce677efb2717f79d9aa0161985326088.png?resize=320x240&vertical=center"
+              }
+            />
+            <ProjectContainer
+              name={"InsideHome"}
+              duration={"4 months"}
+              img={
+                "https://cdn.dribbble.com/userupload/10471938/file/original-2e30b00c968752ab0a1ee42d8a9cd2fa.jpg?resize=320x240&vertical=center"
+              }
+            />
+            <ProjectContainer
+              name={"SaveIt"}
+              duration={"1 month"}
+              img={
+                "https://cdn.dribbble.com/userupload/9784250/file/original-1100bc42ac6b83a633f7e0b0463792bb.png?resize=320x240&vertical=center"
+              }
+            />
+            <ProjectContainer
+              name={"Expensensor"}
+              duration={"4 months"}
+              img={
+                "https://cdn.dribbble.com/userupload/13724215/file/original-4f0c6e43a41ae26badcd7c6a2c736ebb.png?crop=281x63-2165x1476&resize=320x240&vertical=center"
+              }
+            />
+            <ProjectContainer
+              name={"MyBag"}
+              duration={"2 motnhs"}
+              img={
+                "https://cdn.dribbble.com/userupload/14242666/file/still-846073c229981e24a86a3b8d1572d322.gif?resize=320x240&vertical=center"
+              }
+            />
+            <ProjectContainer
+              name={"Remoty"}
+              duration={"4 months"}
+              img={
+                "https://cdn.dribbble.com/userupload/14900913/file/still-b58c7dfa669ede68d7945c220660e112.png?resize=320x240&vertical=center"
+              }
+            />
+          </Reveal>
+        </div>
+      </section>
+
+      {/* Front-end Projects */}
       <section className="w-full h-fit flex items-start justify-strat flex-col gap-2 py-20 px-20 max-md:px-5 pt-32 ">
         <div className="grid grid-cols-2 max-md:grid-cols-1 gap-6 mb-20 max-md:mb-10 max-md:gap-5">
           <Fade triggerOnce>
             <h1 className="text-[37px] leading-[41px] max-md:text-[27px] max-md:leading-7  text-transparent font-semibold tracking-tight z-10 max-w-[400px] ">
               <span className="font-medium text-base text-main-color">
-                Work &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                Concepts &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
                 &nbsp; &nbsp;{" "}
               </span>
               <span className="bg-clip-text bg-gradient-to-br from-black dark:from-white to-stone-800/70 dark:to-stone-400/80">
-                Every Project is Born from a Blank Canvas
+                Front-End Design Concepts and Ideas
               </span>
             </h1>
+            <p className="text-base leading-6 opacity-70">
+              these are website ideas and design concepts made with React. just early drafts and imaginative concepts of mine.
+            </p>
           </Fade>
         </div>
 
