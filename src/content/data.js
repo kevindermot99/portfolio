6f2e86985ada3a0dc454c9abfdba7ce550e6c345
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-03 13:14:06 +0200

    Update data.js

--- src/content/data.js
+++ src/content/data.js
@@ -1,5 +1,5 @@
 
-import cover1 from '../assets/projects/1.png'
+import cover1 from '../assets/projects/11.png'
 
 export const Projects = [
     {
@@ -9,78 +9,8 @@ export const Projects = [
         url: "https://countify.onrender.com",
         logo: "https://countify.onrender.com/assets/logo-02qLKcNb.png",
         duration: "2 Days",
-        tech: ["React js","Tailwind"]
+        tech: ["React js","Tailwind"],
+        color: '#aaaaaa40'
     },
-    {
-        number: '1',
-        name: "Countify",
-        cover: cover1,
-        url: "https://countify.onrender.com",
-        logo: "https://countify.onrender.com/assets/logo-02qLKcNb.png",
-        duration: "2 Days",
-        tech: ["React js","Tailwind"]
-    },
-    {
-        number: '1',
-        name: "Countify",
-        cover: cover1,
-        url: "https://countify.onrender.com",
-        logo: "https://countify.onrender.com/assets/logo-02qLKcNb.png",
-        duration: "2 Days",
-        tech: ["React js","Tailwind"]
-    },
-    {
-        number: '1',
-        name: "Countify",
-        cover: cover1,
-        url: "https://countify.onrender.com",
-        logo: "https://countify.onrender.com/assets/logo-02qLKcNb.png",
-        duration: "2 Days",
-        tech: ["React js","Tailwind"]
-    },
-    {
-        number: '1',
-        name: "Countify",
-        cover: cover1,
-        url: "https://countify.onrender.com",
-        logo: "https://countify.onrender.com/assets/logo-02qLKcNb.png",
-        duration: "2 Days",
-        tech: ["React js","Tailwind"]
-    },
-    {
-        number: '1',
-        name: "Countify",
-        cover: cover1,
-        url: "https://countify.onrender.com",
-        logo: "https://countify.onrender.com/assets/logo-02qLKcNb.png",
-        duration: "2 Days",
-        tech: ["React js","Tailwind"]
-    },
-    {
-        number: '1',
-        name: "Countify",
-        cover: cover1,
-        url: "https://countify.onrender.com",
-        logo: "https://countify.onrender.com/assets/logo-02qLKcNb.png",
-        duration: "2 Days",
-        tech: ["React js","Tailwind"]
-    },
-    {
-        number: '1',
-        name: "Countify",
-        cover: cover1,
-        url: "https://countify.onrender.com",
-        logo: "https://countify.onrender.com/assets/logo-02qLKcNb.png",
-        duration: "2 Days",
-        tech: ["React js","Tailwind"]
-    },
-    {
-        number: '1',
-        name: "Countify",
-        cover: cover1,
-        url: "https://countify.onrender.com",
-        logo: "https://countify.onrender.com/assets/logo-02qLKcNb.png",
-        duration: "2 Days",
-        tech: ["React js","Tailwind"]
-    },
+    
 ]
