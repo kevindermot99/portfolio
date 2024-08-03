0a4ac721076a3653937b26b5c72322551f0a2d79
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-03 11:14:04 +0200

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -95,34 +95,8 @@ function About() {
             </div>
           </Reveal>
         </div>
-      </section>
-
-      <section className="w-full h-full grid grid-cols-2 max-md:grid-cols-1 gap-6 pt-20 px-20 max-md:px-5 max-md:mb-5 max-md:gap-5">
-          <Reveal
-            keyframes={customAnimation}
-            triggerOnce
-            duration={800}
-            cascade
-            damping={0.1}
-            className="w-full"
-            >
-            <div className="text-[37px] leading-[41px] max-md:text-[27px] max-md:leading-7 text-transparent font-semibold tracking-tight z-10 max-w-[400px] ">
-              <p className="font-medium text-lg max-md:text-base text-main-color flex items-center gap-1 max-md:pb-2">
-                <HiBadgeCheck />
-                Completed
-              </p>
-              <p className="bg-clip-text bg-gradient-to-br from-black dark:from-white to-stone-800/70 dark:to-stone-400/80">
-                Complete and Functional Projects
-              </p>
-            </div>
-            <p className="text-base leading-6 opacity-70 text-justify pt-7 max-md:pt-0">
-              Projects that not only look great but also works. This includes everything from full stack web apps to functional React-based apps that perform as intended.
-            </p>
-          </Reveal>
-        </section>
-
+      </section>      
       
-
       {/* Footer */}
       <Footer />
     </div>
