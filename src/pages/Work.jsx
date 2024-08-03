3a5ebc66460ff0851409cae5040ef03e1f86ba88
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-03 19:55:17 +0200

    Update Work.jsx

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -12,6 +12,7 @@ import { Projects } from "../content/data";
 import { HiBadgeCheck } from "react-icons/hi";
 import { FaCompassDrafting } from "react-icons/fa6";
 import { FaDraftingCompass } from "react-icons/fa";
+import Heading from "../components/Heading";
 
 function Work() {
   const customAnimation = keyframes`
@@ -34,27 +35,14 @@ function Work() {
 
       {/* Functional Projects */}
       <section className="w-full h-fit flex items-start justify-strat flex-col gap-2 px-20 max-md:px-5 pt-32 max-md:pt-20 max-w-[1800px] mx-auto">
-        <div className="grid grid-cols-1 max-md:grid-cols-1 gap-2 mb-6 max-md:gap-5">
-          <Reveal
-            keyframes={customAnimation}
-            triggerOnce
-            duration={800}
-            cascade
-            damping={0.1}>
-            <h1 className="text-[37px] leading-[41px] max-md:text-[27px] max-md:leading-7 flex flex-col text-transparent font-semibold tracking-tight z-10 max-w-[400px] ">
-              <span className="font-medium text-lg text-main-color flex items-center gap-1">
-                <FaDraftingCompass />
-                My Work
-              </span>
-              <span className="bg-clip-text bg-gradient-to-br from-black dark:from-white to-stone-800/70 dark:to-stone-400/80">
-                Projects & Contributions
-              </span>
-            </h1>
-            <p className="text-base leading-6 opacity-70">
-              Take a look at the projects I've crafted and those I've collaborated on.
-            </p>
-          </Reveal>
-        </div>
+        
+          <Heading
+            Icon={<FaDraftingCompass />}
+            SectionName={`My Work`}
+            Title={`Projects & Contributions`}
+            Desc={`Take a look at the projects I've crafted and those I've collaborated on.`}
+          />
+        
 
         <div className="w-full grid grid-cols-2 max-lg:grid-cols-1">
           <Reveal
