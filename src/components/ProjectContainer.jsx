1f1eb3850e433b4b35b4fa7c90e45b88dfa7c87c
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-03 09:31:13 +0200

    Update ProjectContainer.jsx

--- src/components/ProjectContainer.jsx
+++ src/components/ProjectContainer.jsx
@@ -6,48 +6,31 @@ import { Link } from "react-router-dom";
 
 function ProjectContainer({ img, name, duration, logo, tech, url }) {
   const techArray = Array.isArray(tech) ? tech : [];
-  // const [url, setUrl] = useState('')
   return (
-    <a href={url} target="_blank" className="flex flex-col items-start justify-start relative w-full max-w-[300px]">
-      {/* external link icon */}
-      {/* <Link
-        to={`/`}
-        className="absolute top-2 right-2 flex items-center justify-center h-auto w-auto p-1 rounded-md shadow-lg bg-white dark:bg-stone-600/40 dark:backdrop-blur-md z-20 group"
-      >
-        <TbExternalLink className="dark:opacity-85 transition" />
-        <span className="text-xs font-medium transition-all ease-in-out duration-500 max-w-0 overflow-clip whitespace-nowrap group-hover:max-w-xs">
-          Live view
-        </span>
-      </Link> */}
-      {/* image */}
-      <div className="group h-fit max-md:h-fit w-full shadow-md cursor-pointer bg-white/80 dark:bg-stone-800/40 rounded-lg mb-1 overflow-clip relative">
-        {/* technologies */}
-        <div className="absolute top-0 left-0 w-full h-full p-2 opacity-0 group-hover:opacity-100 transition ease-in-out duration-300 translate-y-[200px] group-hover:translate-y-0 bg-gradient-to-t from-black/40 to-transparent flex items-end z-10">
-          <div className="flex flex-wrap items-end justify-start h-fit gap-1">
-            {techArray.map((techItem, index) => (
-              <h1 key={index} className="text-xs text-dark-body/80 font-semibold bg-light-body h-[25px] px-3 flex items-center justify-center rounded-full shadow-lg w-fit">
-                {techItem}
-              </h1>
-            ))}
-            
-          </div>
-        </div>
-        <img src={img} className="dark:opacity-90 h-full w-full object-cover group-hover:scale-[1.02] transition duration-500" />
-      </div>
+    <a href={url} target="_blank" className="flex flex-col items-start justify-start gap-6 relative w-full" >
       {/* Details */}
-      <div className="flex justify-between items-center w-full mt-1 gap-6 max-md:gap-5">
-        <h1 className="text-sm z-10 flex items-center gap-1 ">
-          <span className="h-[22px] w-auto aspect-square rounded-full flex">
-            <img src={logo} className="w-full h-full" />
-          </span>
-          <span className="font-SourceSans font tracking-wide text-black/90 dark:text-white/80 max-w-[400px]">
-            {name}
-          </span>
+      <div className="flex justify-between items-end w-full mt-1 gap-5">
+        <h1 className="z-10 flex flex-col text-black/90 dark:text-white gap-3 ">
+          <span className="leading-3 text-2xl font-medium">{name}</span>
+          <span className="font-normal opacity-60 text-sm">May 5, 2023 </span>
+          {/* <div className="flex flex-wrap items-end justify-start h-fit gap-3">
+              {techArray.map((techItem, index) => (
+                <h1 key={index} className="text-sm text-dark-body/60 dark:text-white/60 font-normal ring-1 ring-black/20 h-[30px] dark:ring-white/20 px-3 flex items-center justify-center rounded-full w-fit">
+                  {techItem}
+                </h1>
+              ))}
+
+            </div> */}
         </h1>
-        <p className="text-[13px] leading-6 opacity-70 font-medium">
-          built in {duration}
+        <p className="text-base leading-6 opacity-70 font-normal max-w-[50%] text-justify">
+          Projects that not only React-based apps to full stack web apps.
         </p>
       </div>
+
+      <div className="">
+        <img src={img} className="dark:opacity-90 h-full w-full object-cover rounded-xl transition duration-500" />
+      </div>
+
     </a>
   );
 }
