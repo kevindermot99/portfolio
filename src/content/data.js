8605df4812c08ae40042146d67dce48a26d9fa85
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-03 11:14:02 +0200

    Update data.js

--- src/content/data.js
+++ src/content/data.js
@@ -29,4 +29,58 @@ export const Projects = [
         duration: "2 Days",
         tech: ["React js","Tailwind"]
     },
+    {
+        number: '1',
+        name: "Countify",
+        cover: cover1,
+        url: "https://countify.onrender.com",
+        logo: "https://countify.onrender.com/assets/logo-02qLKcNb.png",
+        duration: "2 Days",
+        tech: ["React js","Tailwind"]
+    },
+    {
+        number: '1',
+        name: "Countify",
+        cover: cover1,
+        url: "https://countify.onrender.com",
+        logo: "https://countify.onrender.com/assets/logo-02qLKcNb.png",
+        duration: "2 Days",
+        tech: ["React js","Tailwind"]
+    },
+    {
+        number: '1',
+        name: "Countify",
+        cover: cover1,
+        url: "https://countify.onrender.com",
+        logo: "https://countify.onrender.com/assets/logo-02qLKcNb.png",
+        duration: "2 Days",
+        tech: ["React js","Tailwind"]
+    },
+    {
+        number: '1',
+        name: "Countify",
+        cover: cover1,
+        url: "https://countify.onrender.com",
+        logo: "https://countify.onrender.com/assets/logo-02qLKcNb.png",
+        duration: "2 Days",
+        tech: ["React js","Tailwind"]
+    },
+    {
+        number: '1',
+        name: "Countify",
+        cover: cover1,
+        url: "https://countify.onrender.com",
+        logo: "https://countify.onrender.com/assets/logo-02qLKcNb.png",
+        duration: "2 Days",
+        tech: ["React js","Tailwind"]
+    },
+    {
+        number: '1',
+        name: "Countify",
+        cover: cover1,
+        url: "https://countify.onrender.com",
+        logo: "https://countify.onrender.com/assets/logo-02qLKcNb.png",
+        duration: "2 Days",
+        tech: ["React js","Tailwind"]
+    },
 ]
