20ec6ef1a7b70a824fd1a471da4448041b08d380
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-04 15:45:25 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -278,6 +278,7 @@ function Home() {
           <div className="w-full h-fit grid grid-cols-4 max-lg:grid-cols-2 max-md:grid-cols-1 gap-3">
             {Feedback.map((comment, index) => (
               <div className="w-full bg-white dark:bg-stone-700/20 p-4 rounded-2xl shadow-sm flex flex-col">
+                <div className="w-full"> </div>
                 <div className="w-full flex items-start justify-between">
                   <div className="flex flex-col">
                     <h1 className="text-sm font-bold">{comment.person}</h1>
@@ -335,8 +336,8 @@ function Home() {
                   </div>
                   <p className="text-xs opacity-50 dark:opacity-70 font-medium ">{comment.date}</p>
                 </div>
-
                 <p className="pt-3 text-sm font-medium ">{comment.comemnt}</p>
+
                 <div className="bg-stone-200/70 dark:bg-stone-700/20 w-full px-3 py-2 mt-3 rounded-xl flex flex-col">
                   <h1 className="text-xs font-medium opacity-50 dark:opacity-70">Commented on Project</h1>
                   <div className="flex items-start gap-2 py-1">
