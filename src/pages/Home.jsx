c497eaf19ea7376b212b6633c509d456ace864cf
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-13 14:45:12 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -57,7 +57,7 @@ function Home() {
             className="flex items-start justify-end flex-col gap-2 w-full"
           >
             <p className="text-base opacity-70 z-10">Hello, I'm Kevin, a 21-year-old from Rwanda, and I am a</p>
-            <h1 className="text-[65px] leading-[70px] max-md:text-[45px] max-md:leading-[50px] text-transparent bg-clip-text bg-gradient-to-br max-w-[600px] from-black dark:from-white to-stone-700/70 dark:to-white/10 font-semibold tracking-tight z-10 ">
+            <h1 className="text-[60px] leading-[65px] max-md:text-[45px] max-md:leading-[50px] text-transparent bg-clip-text bg-gradient-to-br max-w-[600px] from-black dark:from-white to-stone-700/70 dark:to-white/10 font-semibold tracking-tight z-10 ">
               Full-Stack Developer.
             </h1>
             <p className="self-end max-md:self-start text-base font-medium">
