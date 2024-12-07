a1bc206e38b6a7291c8b2b3a7a53c90b53eb7a4b
mbonikevin <mbonimpayekevin@gmail.com>
2024-12-07 10:39:32 +0200

    Update Countify.jsx

--- src/components/Projects/Countify.jsx
+++ src/components/Projects/Countify.jsx
@@ -27,7 +27,7 @@ function Countify() {
       className="w-full h-[312px] bg-[#f2f2f2] dark:bg-card_color_dark transition group break-inside-avoid rounded-3xl relative flex items-center justify-center flex-col overflow-hidden"
     >
       <div
-        className={`flex flex-col gap-2 select-none pointer-events-none group-hover:scale-75 group-hover:translate-y-[-105px] transition-all duration-500 `}
+        className={`flex flex-col gap-2 select-none pointer-events-none xl:group-hover:scale-75 scale-75 xl:group-hover:translate-y-[-105px] group-hover:translate-y-[-105px] transition-all duration-500 `}
       >
         <div className="flex items-center gap-2 ">
           <img
@@ -41,7 +41,7 @@ function Countify() {
       </div>
       {/* screenshot 1 */}
       <div
-        className={`w-[100%] select-none pointer-events-none h-fit px-3 opacity-0 group-hover:opacity-100 absolute left-0 right-0 mx-auto top-[100%] group-hover:top-[35%] delay-150 transition-all duration-500 ease-in-out`}
+        className={`w-[100%] select-none pointer-events-none h-fit px-3 xl:opacity-0 xl:group-hover:opacity-100 absolute left-0 right-0 mx-auto xl:top-[100%] xl:group-hover:top-[35%] top-[35%] delay-150 transition-all duration-500 ease-in-out`}
       >
         <div
           className={`w-full bg-white dark:bg-[#37373b] rounded-2xl flex items-start justify-between gap-0 h-fit p-3 overflow-x-auto invisible_scroll`}
@@ -64,7 +64,7 @@ function Countify() {
         </div>
       </div>
       <div
-        className={`w-full select-none pointer-events-none flex items-start justify-between gap-0 h-fit px-3 opacity-0 group-hover:opacity-100 absolute left-0 right-0 mx-auto top-[100%] group-hover:top-[59%] delay-150 transition-all duration-500 ease-in-out overflow-x-auto invisible_scroll`}
+        className={`w-full select-none pointer-events-none flex items-start justify-between gap-0 h-fit px-3 xl:opacity-0 xl:group-hover:opacity-100 absolute left-0 right-0 mx-auto xl:top-[100%] xl:group-hover:top-[59%] top-[59%] delay-150 transition-all duration-500 ease-in-out overflow-x-auto invisible_scroll`}
       >
         <div
           className={`w-full bg-white dark:bg-[#37373b] rounded-2xl flex items-start justify-between gap-0 h-fit p-3 overflow-x-auto invisible_scroll`}
@@ -79,7 +79,7 @@ function Countify() {
           </div>
         </div>
       </div>
-      {/* screenshot 2 */}
+      {/* screenshot 3 */}
       <div
         className={`w-[100%] px-3 h-fit opacity-0 group-hover:opacity-100 absolute left-0 right-0 mx-auto top-[100%] group-hover:top-[83%] delay-300 transition-all duration-500 ease-in-out`}
       >
