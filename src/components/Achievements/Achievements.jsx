c45beb176998c788e1af55e7d1b99373d5d97f70
mbonikevin <mbonimpayekevin@gmail.com>
2024-12-07 06:48:56 +0200

    Achievements fixed

--- src/components/Achievements/Achievements.jsx
+++ src/components/Achievements/Achievements.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Achievements() {
   return (
-    <div>Achievements</div>
+    <div className='fixed top-0 left-0 bg-white dark:bg-body_dark w-full h-full z-20 p-8 max-lg:p-5'>
+        
+    </div>
   )
 }
 
