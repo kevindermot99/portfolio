8ba8576273fbe4e3d37ecfa4d1242e1c52221003
mbonikevin <mbonimpayekevin@gmail.com>
2024-12-07 10:35:52 +0200

    Update Upfront.jsx

--- src/components/Projects/Upfront.jsx
+++ src/components/Projects/Upfront.jsx
@@ -26,7 +26,7 @@ function Upfront() {
       className="w-full h-[312px] bg-[#f2f2f2] dark:bg-card_color_dark transition group  rounded-3xl relative flex items-center justify-center flex-col overflow-hidden"
     >
       <div
-        className={`flex flex-col gap-2 select-none pointer-events-none group-hover:scale-75 group-hover:translate-y-[-105px] transition-all duration-500 saturate-0 group-hover:saturate-100`}
+        className={`flex flex-col gap-2 select-none pointer-events-none xl:group-hover:scale-75 scale-75 xl:group-hover:translate-y-[-105px] translate-y-[-105px] transition-all duration-500 xl:saturate-0 xl:group-hover:saturate-100`}
       >
         <div className="flex items-center gap-2 ">
           <img src="./project_logos/upfront.png" className="h-12" />
@@ -37,13 +37,13 @@ function Upfront() {
       </div>
       {/* screenshot 1 */}
       <div
-        className={`w-[85%] z-0 h-fit p-3 absolute left-0 right-0 mx-auto top-[100%] group-hover:top-[25%] delay-150 transition-all duration-500 ease-in-out`}
+        className={`w-[85%] z-0 h-fit p-3 absolute left-0 right-0 mx-auto xl:top-[100%] xl:group-hover:top-[25%] top-[25%] delay-150 transition-all duration-500 ease-in-out`}
       >
         <UpfrontCard />
       </div>
       {/* screenshot 2 */}
       <div
-        className={`w-[100%] z-10 h-fit p-3 opacity-0 group-hover:opacity-100 absolute left-0 right-0 select-none pointer-events-none mx-auto top-[100%] group-hover:top-[35%] delay-300 transition-all duration-500 ease-in-out`}
+        className={`w-[100%] z-10 h-fit p-3 xl:opacity-0 xl:group-hover:opacity-100 absolute left-0 right-0 select-none pointer-events-none mx-auto xl:top-[100%] xl:group-hover:top-[35%] top-[35%] delay-300 transition-all duration-500 ease-in-out`}
       >
         <UpfrontCard />
       </div>
