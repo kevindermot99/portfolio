89a4acbe903448d6d3b2cbca3e88e39931bac61a
mbonikevin <mbonimpayekevin@gmail.com>
2024-12-07 10:03:40 +0200

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -25,13 +25,13 @@ function Navbar() {
   const [activeSection, setActiveSection] = useState(null);
   const [showCerts, setShowCerts] = useState(false);
   return (
-    <div className="w-[250px] bg-orange-400/0 flex flex-col items-start justify-between h-full text-dark_color dark:text-light_color fixed z-10 top-0 left-0 px-11 py-8 max-lg:hidden">
+    <div className="w-[250px] bg-orange-400/0 flex flex-col items-start justify-between h-full text-dark_color dark:text-light_color fixed z-10 top-0 left-0 px-11 py-8 max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:flex-row">
       {showCerts && <Certificates setShowCerts={setShowCerts} />}
       <Reveal triggerOnce duration={500} keyframes={customAnimation}>
         <h1 className="text-[24px] leading-[24px] w-fit flex items-center cursor-default select-none">
           Kevin
         </h1>
-        <div className="flex-1 h-fit flex flex-col gap-1 w-full">
+        <div className="flex-1 h-fit flex flex-col gap-1 w-full max-lg:hidden">
           <ScrollTo
             activeSection={activeSection}
             setActiveSection={setActiveSection}
@@ -64,7 +64,7 @@ function Navbar() {
             resume
           </a>
         </div>
-        <div className="h-fit flex flex-col bg-orange-200/0 gap-2 w-full">
+        <div className="h-fit flex flex-col bg-orange-200/0 gap-2 w-full max-lg:w-fit max-lg:flex-row">
           <div className="flex items-center gap-2 relative">
             <a
               href="https://github.com/kevindermot99"
