b3a3383c203465518bc5bc541a3a2d9e33b81335
mbonikevin <mbonimpayekevin@gmail.com>
2024-12-07 09:11:46 +0200

    Update Upfront.jsx

--- src/components/Projects/Upfront.jsx
+++ src/components/Projects/Upfront.jsx
@@ -21,7 +21,7 @@ function Upfront() {
 
   return (
     <Link
-      to="/"
+      to="https://upfront.onrender.com/"
       target="_blank"
       className="w-full h-[312px] bg-[#f2f2f2] dark:bg-card_color_dark transition group  rounded-3xl relative flex items-center justify-center flex-col overflow-hidden"
     >
