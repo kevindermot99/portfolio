0115e0badca4ac09e623352007bcb39b981dcf9b
mbonikevin <mbonimpayekevin@gmail.com>
2024-12-07 07:15:43 +0200

    achievements filters maped

--- src/components/Achievements/Achievements.jsx
+++ src/components/Achievements/Achievements.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Reveal from "react-awesome-reveal";
 import { keyframes } from "@emotion/react";
 import { PiCertificateBold } from "react-icons/pi";
@@ -17,6 +17,8 @@ to{
 `;
 
 function Achievements() {
+  const certificateFilters = ["All", "Technical", "Soft Skills", "Education"];
+  const [type, setType] = useState("All")
   return (
     <div className="fixed top-0 left-0 bg-white dark:bg-body_dark w-full h-full z-20 p-8 max-lg:p-5 overflow-y-auto text-dark_color dark:text-light_color">
       <Reveal duration={500} keyframes={customAnimation}>
@@ -26,8 +28,15 @@ function Achievements() {
           </button>
         </div>
         {/* hero */}
-        <div className="w-full h-fit flex flex-col items-start justify-start py-[30px]">
-            <h1 className="font-Custom_b text-5xl tracking-tight">Achievements</h1>
+        <div className="w-full h-fit flex flex-col items-start justify-start gap-6 py-[30px]">
+          <h1 className="text-5xl font-Custom_b tracking-tight">Achievements</h1>
+          <div className="flex items-center justify-start gap-6">
+            {certificateFilters.map((filter, index) => (
+              <h1 onClick={() =>  setType(fil)} key={index} className={`border-b-[2px] cursor-pointer hover:text-dark_color dark:hover:text-light_color ${type === filter ? 'text-dark_color dark:text-light_color border-dark_color dark:border-light_color' : 'text-dark_color_weak dark:text-light_color_weak border-transparent'}`}>
+                {filter}
+              </h1>
+            ))}
+          </div>
         </div>
       </Reveal>
     </div>
