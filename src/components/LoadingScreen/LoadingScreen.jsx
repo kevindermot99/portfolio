3228d798305b3b0eecf0c4d7c54c93aa5547038b
mbonikevin <mbonimpayekevin@gmail.com>
2024-12-07 10:03:38 +0200

    Update LoadingScreen.jsx

--- src/components/LoadingScreen/LoadingScreen.jsx
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -12,15 +12,15 @@ function LoadingScreen() {
   }, []);
   return (
     <div
-      className={`wrapper p-16 bg-white dark:bg-body_dark text-dark_color dark:text-light_color transition-all duration-500 ${
+      className={`wrapper p-16 max-lg:p-5 bg-white dark:bg-body_dark text-dark_color dark:text-light_color transition-all duration-500 ${
         hide ? "opacity-0" : "opacity-100"
       }`}
     >
       <Fade triggerOnce cascade={0.4} damping={0.2} delay={300}>
-        <h1 className={`text-[80px] font-Custom_m leading-[95px] max-2xl:text-[60px] max-2xl:leading-[60px] animateHide`}>
+        <h1 className={`text-[80px] max-lg:text-4xl font-Custom_m leading-[95px] max-lg:leading-10 max-2xl:text-[60px] max-2xl:leading-[60px] animateHide`}>
           Kevin K. Mbonimpaye
         </h1>
-        <h1 className={`text-[30px] font-Custom_r leading-[50px] flex items-center gap-0 animateHide`}>
+        <h1 className={`text-[30px] max-lg:text-base max-lg:mt-3 leading-[50px] max-lg:leading-5 flex items-center gap-0 animateHide`}>
           Full<span className="font-Inter">-</span>stack
           Developer
         </h1>
