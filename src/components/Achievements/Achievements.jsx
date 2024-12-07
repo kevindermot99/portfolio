303f5c343624bf587c15b0b5660b4aedb89b644f
mbonikevin <mbonimpayekevin@gmail.com>
2024-12-07 07:01:10 +0200

    Update Achievements.jsx

--- src/components/Achievements/Achievements.jsx
+++ src/components/Achievements/Achievements.jsx
@@ -18,7 +18,7 @@ to{
 
 function Achievements() {
   return (
-    <div className="fixed top-0 left-0 bg-white dark:bg-body_dark w-full h-full z-20 p-8 max-lg:p-5 overflow-y-auto">
+    <div className="fixed top-0 left-0 bg-white dark:bg-body_dark w-full h-full z-20 p-8 max-lg:p-5 overflow-y-auto text-dark_color dark:text-light_color">
       <Reveal duration={500} keyframes={customAnimation}>
         <div className="w-full flex items-center justify-end">
           <button className="text-dark_color dark:text-light_color dark:bg-card_color_dark peer bg-[#f2f2f2] active:bg-stone-200 dark:active:bg-[#35353a] w-[44px] min-w-[44px] h-auto flex items-center justify-center text-xl aspect-square rounded-full">
@@ -26,7 +26,9 @@ function Achievements() {
           </button>
         </div>
         {/* hero */}
-        
+        <div className="w-full h-fit flex flex-col items-start justify-start py-[30px]">
+            <h1 className="font-Custom_b text-5xl tracking-tight">Achievements</h1>
+        </div>
       </Reveal>
     </div>
   );
