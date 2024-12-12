cda13b4adcb74f6d01fcc89679e74535a1a8aa58
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-12 07:15:18 +0200

    Stalker-commit: src/components/Projects/NewProject.jsx

--- src/components/Projects/NewProject.jsx
+++ src/components/Projects/NewProject.jsx
@@ -7,7 +7,9 @@ function NewProject() {
       <div className="w-full h-full border-[2px] border-dashed border-dark_color_weak/50 dark:border-card_color_light/10 text-base text-dark_color/40 dark:text-light_color_weak rounded-3xl flex items-center justify-center overflow-hidden break-inside-avoid">
         New project soon..
       </div>
-      <div className="px-2 bg-transparent h-[30px] text-[13px] py-1 text-transparent"></div>
+      <p className="px-2 text-[13px] py-1 text-dark_color/40 dark:text-light_color_weak">
+        {ProjectTypes.Personal}
+      </p>
     </div>
   );
 }
