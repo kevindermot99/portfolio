356b04e5ee751b033a594248323f74acd412d96d
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-12 07:09:48 +0200

    Stalker-commit: src/components/Projects/NewProject.jsx

--- src/components/Projects/NewProject.jsx
+++ src/components/Projects/NewProject.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function NewProject() {
   return (
-    <div className="w-full h-[full] row-span-2  border-[2px] border-dashed border-dark_color_weak dark:border-card_color_light/10 text-base text-dark_color dark:text-light_color_weak rounded-3xl flex items-center justify-center overflow-hidden break-inside-avoid">
+    <div className="w-full h-[full] border-[2px] border-dashed border-dark_color_weak dark:border-card_color_light/10 text-base text-dark_color dark:text-light_color_weak rounded-3xl flex items-center justify-center overflow-hidden break-inside-avoid">
       New project soon..
     </div>
   );
