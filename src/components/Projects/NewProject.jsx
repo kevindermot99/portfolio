b78eda004b7804a31132de2617b1df16a3ac6058
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-12 07:14:18 +0200

    Stalker-commit: src/components/Projects/NewProject.jsx

--- src/components/Projects/NewProject.jsx
+++ src/components/Projects/NewProject.jsx
@@ -3,12 +3,11 @@ import { ProjectTypes } from "./Types";
 
 function NewProject() {
   return (
-    <div className="w-full h-full flex items-start flex-col break-inside-avoid">
+    <div className="w-full h-full row-span-2 flex items-start flex-col break-inside-avoid">
       <div className="w-full h-full border-[2px] border-dashed border-dark_color_weak dark:border-card_color_light/10 text-base text-dark_color dark:text-light_color_weak rounded-3xl flex items-center justify-center overflow-hidden break-inside-avoid">
         New project soon..
       </div>
-      <div className="px-2 bg-red-400 h-[30px] text-[13px] py-1 text-dark_color/40 dark:text-light_color_weak">
-      </div>
+      <div className="px-2 bg-transparent h-[30px] text-[13px] py-1 text-transparent"></div>
     </div>
   );
 }
